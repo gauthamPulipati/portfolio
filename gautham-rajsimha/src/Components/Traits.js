f5011ba77b39ededd295a824b1201a06a90b7561
Gautham Rajsimha Pulipati <pulipati.g@gmail.com>
2021-11-21 07:27:44 -0500

    fixed fonts

--- gautham-rajsimha/src/Components/Traits.js
+++ gautham-rajsimha/src/Components/Traits.js
@@ -5,23 +5,49 @@ import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 
 const styles = {
-    creativetext: {
+    creativetext1: {
         display: 'flex',
         justifyContent: 'center',
-        height: '560px',
+        alignItems:'center',
+        height: '490px',
         fontSize: '30px',
         fontFamily: 'Andale Mono, monospace',
         fontWeight: 'bold',
         backgroundColor: '#030606',
         color: 'white',
         borderRadius: '1%',
-        paddingTop: '200px',
+        paddingLeft: '30px'
+    },
+    creativetext2: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems:'center',
+        height: '500px',
+        fontSize: '30px',
+        fontFamily: 'Andale Mono, monospace',
+        fontWeight: 'bold',
+        backgroundColor: '#030606',
+        color: 'white',
+        borderRadius: '1%',
+        paddingLeft: '30px'
+    },
+    creativetext3: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems:'center',
+        height: '340px',
+        fontSize: '30px',
+        fontFamily: 'Andale Mono, monospace',
+        fontWeight: 'bold',
+        backgroundColor: '#030606',
+        color: 'white',
+        borderRadius: '1%',
         paddingLeft: '30px'
     },
     growthimg: {
         width: '100%',
         borderRadius: '1%',
-        height: '100%'
+        height: 'auto'
     },
     cardcolor: {
         display: 'flex',
@@ -49,10 +75,10 @@ function Trait() {
                 </Card>
             </Grid>
             <Grid item xs={12} md={6} style={{ height: '100%' }}>
-                <div style={styles.creativetext}>I dive into my inner pool of knowledge and insight to populate my mind with unique and elegant solutions</div>
+                <div style={styles.creativetext1}>I dive into my inner pool of knowledge and insight to populate my mind with unique and elegant solutions</div>
             </Grid>
             <Grid item xs={12} md={6} style={{ height: '100%s' }}>
-                <div style={styles.creativetext}>When you go from fixed mindset to growth mindset, a new world of possibilities open up</div>
+                <div style={styles.creativetext2}>When you go from fixed mindset to growth mindset, a new world of possibilities open up</div>
             </Grid>
             <Grid item xs={12} md={6}>
                 <img src={process.env.PUBLIC_URL + '/growth.jpeg'} style={styles.growthimg} />
@@ -61,7 +87,7 @@ function Trait() {
                 <img src={process.env.PUBLIC_URL + '/team.jpg'} style={styles.growthimg} />
             </Grid>
             <Grid item xs={12} md={6} style={{ height: '100%' }}>
-                <div style={styles.creativetext}>The way to achieve your own success is to be willing to help somebody else get to it first</div>
+                <div style={styles.creativetext3}>The way to achieve your own success is to be willing to help somebody else get to it first</div>
             </Grid>
         </React.Fragment>
     )
